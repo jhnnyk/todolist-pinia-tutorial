0cd335fe30e62bc35eb1e2fda889ab611b961fef
John Knotts <jhnnyk@gmail.com>
2025-03-22 17:48:31 -0600

    deleting items and styles

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -4,7 +4,7 @@ import { storeToRefs } from 'pinia'
 
 const store = useTodoListStore()
 const { todoList } = storeToRefs(store)
-const { toggleCompleted } = store
+const { toggleCompleted, deleteTodo } = store
 </script>
 
 <template>
@@ -12,11 +12,27 @@ const { toggleCompleted } = store
     <div class="content">
       <span :class="{ completed: todo.completed }">{{ todo.item }}</span>
       <span @click.stop="toggleCompleted(todo.id)">&#10004;</span>
+      <span @click="deleteTodo(todo.id)" class="x">&#10060;</span>
     </div>
   </div>
 </template>
 
 <style scoped>
+span {
+  margin: 0 10px;
+  cursor: pointer;
+}
+.item {
+  display: flex;
+  justify-content: center;
+}
+.content {
+  display: flex;
+  font-size: 1.5em;
+  justify-content: space-between;
+  width: 80vw;
+  padding: 5px;
+}
 .completed {
   text-decoration: line-through;
 }
