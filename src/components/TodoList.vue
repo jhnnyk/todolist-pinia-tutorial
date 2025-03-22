759b5a80eb3bfa9e5e7810c20a459a8e3cb5a2bd
John Knotts <jhnnyk@gmail.com>
2025-03-22 17:24:45 -0600

    display todos

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,17 @@
-<script setup></script>
+<script setup>
+import { useTodoListStore } from '@/stores/todoList'
+import { storeToRefs } from 'pinia'
+
+const store = useTodoListStore()
+const { todoList } = storeToRefs(store)
+</script>
 
 <template>
-  <div></div>
+  <div v-for="todo in todoList" :key="todo.id" class="item">
+    <div class="content">
+      <span>{{ todo.item }}</span>
+    </div>
+  </div>
 </template>
 
 <style></style>
